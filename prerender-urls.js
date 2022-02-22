30a70b847ab2b986bacf4b3a2570c55d50636712
LordDashMe <reyesjoshuaclifford@gmail.com>
2022-02-22 19:33:22 +0800

    build: add fix routing

--- prerender-urls.js
+++ prerender-urls.js
@@ -1,10 +1,10 @@
 module.exports = function () {
   return [
     {
-      url: (typeof process !== 'undefined' && process.env.ENVIRONMENT === 'production' ? '/axie-tracker-dashboard/' : '/'),
+      url: '/',
       title: 'Axie Tracker Dashboard',
       description: 'A simplified dashboard for your axie account stats.',
-      canonical_url: (typeof process !== 'undefined' && process.env.ENVIRONMENT === 'production' ? '/axie-tracker-dashboard/' : '/')
+      canonical_url: '/'
     }
   ];
 };
